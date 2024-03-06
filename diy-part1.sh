1ce8374997cad929c5e711f6b5e300daf71cd28b
uingei <uingei@163.com>
2024-03-07 07:04:24 +0800

    for rt-acrh17

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+#echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev' >> feeds.conf.default
+echo 'src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
+echo 'src-git openclash_luci https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >> feeds.conf.default
+echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git;master' >> feeds.conf.default
